1ec3a2039f652e6409d525d609c27609f31fdaf7
Brandon Simms <brandons@iceboxmail.com>
2017-11-27 10:33:19 -0500

    AddedUI

---
+++ ts/IBJS.Essent.UI.ts
@@ -0,0 +1,28 @@
+/***************************************
+ * IBJS v.01 - Icebox Javascript 
+ * for use within Icebox Projects only
+ * 
+ * created by Brandons Simms
+ ***************************************/
+
+
+$( document ).ready(function() {
+    
+        var ibLogin = $('.ib-login');
+        var essentLogin = $('.LoginLinks');
+        var userName = $('span[id*="_lblWelcome"]').length > 0 ? $('span[id*="_lblWelcome"]')[0].innerText.slice(10).slice(0, -3) : false;
+        var loginStr = userName ? '<a href="/login.html?logout"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a><br><span id="userName">Hi,' + userName + '!</span>' : '<a href="/login.html"><i class="fa fa-sign-in" aria-hidden="true"></i>Login</a>';
+        ibLogin.length && ibLogin.html(loginStr).append(essentLogin.addClass('ib-drop').css('display', 'none'));
+    
+        $('.nav-drop').on("mouseover", function() {
+            $(this).addClass('is-active');
+            $(this).find('.ib-drop').css('display', 'block');
+        });
+    
+        $('.nav-drop').on("mouseout", function() {
+            $(this).removeClass('is-active');
+            $(this).find('.ib-drop').css('display', 'none');
+        });
+    });
+    
+     console.log('IBJS Essent UI loaded...');
--- ts/modules/access.ts
+++ ts/modules/access.ts
@@ -1,3 +1,22 @@
 module Access {
-    
+    export function validateUser(event, email, pass): boolean | void {
+        $.ajax({
+            url: "https://icebox-coolstuff.herokuapp.com/sw/varify",
+            type: "POST",
+            crossDomain: true,
+            data: { email: email, site: 'site' },
+            dataType: "json",
+            success: function(response) {
+                if (response.success === true) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+            error: function(xhr, status) {
+                alert(status);
+                return false;
+            }
+        });
+    }
 }
