d8a5b9c72ef31f72d960ff1fa388be8272f59a89
Brandon Simms <brandons@iceboxmail.com>
2017-11-27 10:34:28 -0500

    UI_Update

--- ts/IBJS.Essent.UI.ts
+++ ts/IBJS.Essent.UI.ts
@@ -11,7 +11,7 @@ $( document ).ready(function() {
         var ibLogin = $('.ib-login');
         var essentLogin = $('.LoginLinks');
         var userName = $('span[id*="_lblWelcome"]').length > 0 ? $('span[id*="_lblWelcome"]')[0].innerText.slice(10).slice(0, -3) : false;
-        var loginStr = userName ? '<a href="/login.html?logout"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a><br><span id="userName">Hi,' + userName + '!</span>' : '<a href="/login.html"><i class="fa fa-sign-in" aria-hidden="true"></i>Login</a>';
+        var loginStr = userName ? '<a href="/login.html?logout"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a>' : '<a href="/login.html"><i class="fa fa-sign-in" aria-hidden="true"></i>Login</a>';
         ibLogin.length && ibLogin.html(loginStr).append(essentLogin.addClass('ib-drop').css('display', 'none'));
     
         $('.nav-drop').on("mouseover", function() {
--- ts/IBJS.essent.ts
+++ ts/IBJS.essent.ts
@@ -11,23 +11,4 @@
 /// <reference path="./modules/errors.ts" />
 /// <reference path="./modules/notifications.ts" />
 
-$( document ).ready(function() {
-
-    var ibLogin = $('.ib-login');
-    var essentLogin = $('.LoginLinks');
-    var userName = $('span[id*="_lblWelcome"]').length > 0 ? $('span[id*="_lblWelcome"]')[0].innerText.slice(10).slice(0, -3) : false;
-    var loginStr = userName ? '<a href="/login.html?logout"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a><br><span id="userName">Hi,' + userName + '!</span>' : '<a href="/login.html"><i class="fa fa-sign-in" aria-hidden="true"></i>Login</a>';
-    ibLogin.length && ibLogin.html(loginStr).append(essentLogin.addClass('ib-drop').css('display', 'none'));
-
-    $('.nav-drop').on("mouseover", function() {
-        $(this).addClass('is-active');
-        $(this).find('.ib-drop').css('display', 'block');
-    });
-
-    $('.nav-drop').on("mouseout", function() {
-        $(this).removeClass('is-active');
-        $(this).find('.ib-drop').css('display', 'none');
-    });
-});
-
  console.log('IBJS Essent loaded...');
