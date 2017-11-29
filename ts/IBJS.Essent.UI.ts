23d5d81997d1aa6966ef9499ec301a697d56102e
Brandon Simms <brandons@iceboxmail.com>
2017-11-29 15:17:35 -0500

    ButtonUpdate

--- ts/IBJS.Essent.UI.ts
+++ ts/IBJS.Essent.UI.ts
@@ -11,7 +11,7 @@ $( document ).ready(function() {
     var ibLogin = $('.ib-account-signin');
     var essentLogin = $('.LoginLinks').css('display','none');
     var userName = $('span[id*="_lblWelcome"]').length > 0 ? $('span[id*="_lblWelcome"]')[0].innerText.slice(10).slice(0, -3) : false;
-    var loginStr = userName ? '<a href="/login.html?logout"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a>' : '<a href="/login.html"><i class="fa fa-sign-in" aria-hidden="true"></i>Login</a>';
+    var loginStr = userName ? '<a href="/login.html?logout"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a>' : '<a href="/login.html" class="cw-btn cw-btn--action cw-btn--full js-header-sign-in"><i class="fa fa-sign-in" aria-hidden="true"></i>Login</a>';
     ibLogin.length && ibLogin.html(loginStr).append(essentLogin);
 
     $('.nav-drop').on("mouseover", function() {
