411483b49f9f99987cc74cabe5a7050dea6a0ef1
Brandon Simms <brandons@iceboxmail.com>
2017-12-01 14:55:15 -0500

    LogoutFix

--- ts/IBJS.Essent.UI.ts
+++ ts/IBJS.Essent.UI.ts
@@ -11,7 +11,7 @@ $( document ).ready(function() {
     var ibLogin = $('.ib-account-signin');
     var essentLogin = $('.LoginLinks').css('display','none');
     var userName = $('span[id*="_lblWelcome"]').length > 0 ? $('span[id*="_lblWelcome"]')[0].innerText.slice(10).slice(0, -3) : false;
-    var loginStr = userName ? '<a href="/login.html?logout"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a>' : '<a href="/login.html" class="ib-btn ib-btn--action ib-btn--full js-header-sign-in"><i class="fa fa-sign-in" aria-hidden="true"></i>Login</a>';
+    var loginStr = userName ? '<a href="/Login.html?action=logout"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a>' : '<a href="/login.html" class="ib-btn ib-btn--action ib-btn--full js-header-sign-in"><i class="fa fa-sign-in" aria-hidden="true"></i>Login</a>';
     ibLogin.length && ibLogin.html(loginStr);
 
     $('.nav-drop').on("mouseover", function() {
