bc2ed8c36b6a1c5e58102b6498014863bbf9c50a
Brandon Simms <brandons@iceboxmail.com>
2017-12-03 22:29:46 -0500

    MiniCart

--- ts/IBJS.Essent.UI.ts
+++ ts/IBJS.Essent.UI.ts
@@ -13,13 +13,23 @@ $( document ).ready(function() {
     var ibLogin = $('.ib-account-signin');
     var essentLogin = $('.LoginLinks');
     $('.LoginLinks a[id*="_lnkLogIn"]').html('<i class="fa fa-sign-in" aria-hidden="true"></i>Login').addClass('ib-btn ib-btn--action ib-btn--full js-header-sign-in');
+
     var userName = $('span[id*="_lblWelcome"]').length > 0 ? $('span[id*="_lblWelcome"]')[0].innerText.slice(10).slice(0, -3) : false;
+
     userName ? $('.LoginLinks').html('<a href="/Login.html?action=logout"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a>').addClass('ib-btn ib-btn--action ib-btn--full js-header-sign-in') 
         : $('.LoginLinks a[id*="_lnkLogIn"]').html('<i class="fa fa-sign-in" aria-hidden="true"></i>Login').addClass('ib-btn ib-btn--action ib-btn--full js-header-sign-in');
     ibLogin.length && ibLogin.append(essentLogin);
 
-    // Search ---------------------------------
+    // SearchBar ------------------------------
     $('#header-searchform').append($('.productSearchInput'));
+
+    // Mini Cart ----------------------------
+
+    var itemCount: number = 0;
+    $(".CartInformation .CartTable .DisplayTextBox").each(function() {
+        itemCount = itemCount + Number(this.value);
+    });
+    $('.quantity').html(itemCount.toString());
 
     // Navigation -----------------------------
     $('.nav-drop').on("mouseover", function() {
