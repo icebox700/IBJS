e418ebd3bfa6e19440024303b36c6fa5dba38312
Brandon Simms <brandons@iceboxmail.com>
2017-12-05 11:05:35 -0500

    AddingBrowserCheck

--- ts/IBJS.Essent.UI.ts
+++ ts/IBJS.Essent.UI.ts
@@ -8,45 +8,48 @@
 
 //For Dev Only! --- Remove and upload when going to prod.
 $.getScript( "https://dl.dropbox.com/s/a50yxh39wsqwink/masterPage.js?dl=0" )
-.done(function( script, textStatus ) {
+    .done(function( script, textStatus ) {
 });
 
-const Logged_Out_MiniCart: string = `<div class="cart-empty">
-<h3>Your cart is empty.</h3>
-<p>Something missing? <a href="/Login.html">Sign in</a> to see items you may have added from another computer or device.</p>
-</div>`;
-const Logged_In_MiniCart: string = `<div class="cart-empty">
-<h3>Your cart is empty.</h3>
-</div>`;
+const Logged_Out_MiniCart: string = `
+    <div class="cart-empty">
+        <h3>Your cart is empty.</h3>
+        <p>Something missing? <a href="/Login.html">Sign in</a> to see items you may have added from another computer or device.</p>
+    </div>`;
+const Logged_In_MiniCart: string = `
+    <div class="cart-empty">
+        <h3>Your cart is empty.</h3>
+    </div>`;
 
 $( document ).ready(function() {
+    
+    // Var
+    let itemCount: number = 0;
+    let ibLogin = $('.ib-account-signin');
+    let essentLogin = $('.LoginLinks');
+    let userName = $('span[id*="_lblWelcome"]').length > 0 ? $('span[id*="_lblWelcome"]')[0].innerText.slice(10).slice(0, -3).split(/\s+/) : false;
+    let login_buttons = $('.LoginLinks a[id*="_lnkLogIn"]')
+        .html('<i class="fa fa-sign-in" aria-hidden="true"></i>Login')
+        .addClass('ib-btn ib-btn--action ib-btn--full js-header-sign-in');
+    let logout_button = $('.LoginLinks a[id*="_lnkNewAccount"]')
+        .html('<i class="fa fa-sign-out" aria-hidden="true"></i>Logout')
+        .addClass('ib-btn ib-btn--action ib-btn--full js-header-sign-in');
 
     // Mini Cart ----------------------------
-    let itemCount: number = 0;
     $('.CartInformation .CartTable .DisplayTextBox').each(function() {
         itemCount = itemCount + Number(this.value);
     });
     itemCount ? $('#minicart .cart-empty').replaceWith($('.CartInformation')) : $('.CartInformation').css('display', 'none');
     $('.quantity').html(itemCount.toString());
  
-    // Login ----------------------------------
-    let ibLogin = $('.ib-account-signin');
-    let essentLogin = $('.LoginLinks');
-    let userName = $('span[id*="_lblWelcome"]').length > 0 ? $('span[id*="_lblWelcome"]')[0].innerText.slice(10).slice(0, -3).split(/\s+/) : false;
-    
-    let login_buttons = $('.LoginLinks a[id*="_lnkLogIn"]')
-        .html('<i class="fa fa-sign-in" aria-hidden="true"></i>Login')
-        .addClass('ib-btn ib-btn--action ib-btn--full js-header-sign-in');
+    // Login ---------------------------------
 
-    if (userName) {    
+    if (userName) {
         $('span[id*="_lblWelcome"]').hide();
         $('span[id*="_lblDivider"]').hide();
         $('.top-nav-account').addClass('account-logged');
         $('.top-nav-username').html(`Hi, ${userName[0]}`);
         login_buttons.hide();
-        $('.LoginLinks a[id*="_lnkNewAccount"]')
-            .html('<i class="fa fa-sign-out" aria-hidden="true"></i>Logout')
-            .addClass('ib-btn ib-btn--action ib-btn--full js-header-sign-in');
 
         itemCount || $('#minicart').html(Logged_In_MiniCart);
     } else { 
@@ -75,5 +78,5 @@ $( document ).ready(function() {
         $('.nav-drop-main .ib-drop').css('display', 'block');
     });
 });
-    
-     console.log('IBJS Essent UI loaded...');
+
+console.log('IBJS Essent UI loaded...');
--- ts/IBJS.essent.ts
+++ ts/IBJS.essent.ts
@@ -15,6 +15,7 @@ let phone: number = 480;
 let tablet: number = 820;
 let desktop: number = 1280;
 
+// Set/Update Body Class
 const device_adj = (win) => {
     if (win.width() <= phone && win.width() > 0) { $('body').attr('class', 'bd-home is-phone'); }
     if (win.width() <= tablet && win.width() > phone) { $('body').attr('class', 'bd-home is-tablet'); }
@@ -24,6 +25,17 @@ const device_adj = (win) => {
 device_adj($(window)).on('resize', function(){
     device_adj($(this));
 });
+
+// Browser Check
+let Browser = (): String => {
+    if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) return 'Opera'
+    else if(navigator.userAgent.indexOf("Chrome") != -1 ) return 'Chrome'
+    else if(navigator.userAgent.indexOf("Safari") != -1) return 'Safari'
+    else if(navigator.userAgent.indexOf("Firefox") != -1 ) return 'Firefox'
+    else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) return 'IE'
+    else return 'unknown'
+};
+console.log(Browser());
 
 //  Remove Essent Footer
 $('document').ready(function() {
