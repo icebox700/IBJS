900694791aeda2d6ae0b4d45d7320bb654a38ebc
Brandon Simms <brandons@iceboxmail.com>
2017-12-05 11:17:21 -0500

    browserUpdate

--- ts/IBJS.essent.ts
+++ ts/IBJS.essent.ts
@@ -27,15 +27,24 @@ device_adj($(window)).on('resize', function(){
 });
 
 // Browser Check
-let Browser = (): String => {
-    if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) return 'Opera'
-    else if(navigator.userAgent.indexOf("Chrome") != -1 ) return 'Chrome'
-    else if(navigator.userAgent.indexOf("Safari") != -1) return 'Safari'
-    else if(navigator.userAgent.indexOf("Firefox") != -1 ) return 'Firefox'
-    else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) return 'IE'
-    else return 'unknown'
-};
-console.log(Browser());
+const get_browser = () => {
+    let ua = navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
+    if(/trident/i.test(M[1])){
+        tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
+        return {name:'IE',version:(tem[1]||'')};
+        }   
+    if(M[1]==='Chrome'){
+        tem=ua.match(/\bOPR|Edge\/(\d+)/)
+        if(tem!=null)   {return {name:'Opera', version:tem[1]};}
+        }   
+    M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+    return {
+      name: M[0],
+      version: M[1]
+    };
+ }
+console.log(get_browser());
 
 //  Remove Essent Footer
 $('document').ready(function() {
