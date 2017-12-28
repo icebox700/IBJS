ab592c1334050cfe5f433525c535952fbcc0f4e1
Brandon Simms <brandons@iceboxmail.com>
2017-12-28 15:48:35 -0500

    Another Test

--- ts/IBJS.ts
+++ ts/IBJS.ts
@@ -5,79 +5,51 @@
  * created by Brandons Simms
  ***************************************/
 
+class IBJS {
+    constructor() {
+        console.log('testing')
+      };
+    public body: JQuery = $('body');
+    public site: String = window.location.host;
+}
 
-window.IBJS = (function () {
-    function IbJs (els) {
-         
+class Navigation extends IBJS {
+    public backtrack = () => {
+        window.history.back();
+    }
+}
+
+class Cookies extends IBJS {
+
+    public createCookie = (name: string, value, days: number): void => {
+        let expires: string;
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = `; expires=${date.toUTCString()}`
+        }
+        document.cookie = `${name}=${value + expires}; path=/`;
     }
 
-    var IBJS = {
-        body: $('body'),
-        
-        site: window.location.host,
+    public readCookie = (name: string): string | null => {
+        var nameEQ: string = `${name}=`;
+        var ca: string[] = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
 
-        get: function (selector) {
-         
-        }   
-    };
+    public eraseCookie = (name: string): void => {
+        this.createCookie(name, "", -1);
+    }
 
-    // class IBJS {
-    //     constructor() {
-    //         console.log('testing')
-    //         };
-    //     public body: JQuery = $('body');
-    //     public site: String = window.location.host;
-    // }
-     
-    return IBJS;
-}());
-
-
-// class IBJS {
-//     constructor() {
-//         console.log('testing')
-//       };
-//     public body: JQuery = $('body');
-//     public site: String = window.location.host;
-// }
-
-// class Navigation extends IBJS {
-//     public backtrack = () => {
-//         window.history.back();
-//     }
-// }
-
-// class Cookies extends IBJS {
-
-//     public createCookie = (name: string, value, days: number): void => {
-//         let expires: string;
-//         if (days) {
-//             var date = new Date();
-//             date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-//             expires = `; expires=${date.toUTCString()}`
-//         }
-//         document.cookie = `${name}=${value + expires}; path=/`;
-//     }
-
-//     public readCookie = (name: string): string | null => {
-//         var nameEQ: string = `${name}=`;
-//         var ca: string[] = document.cookie.split(';');
-//         for (var i = 0; i < ca.length; i++) {
-//             var c = ca[i];
-//             while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-//             if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-//         }
-//         return null;
-//     }
-
-//     public eraseCookie = (name: string): void => {
-//         this.createCookie(name, "", -1);
-//     }
-
-//     public getAllCookies = (): string[] => {
-//         return document.cookie.split(';');
-//     }
-// }
+    public getAllCookies = (): string[] => {
+        return document.cookie.split(';');
+    }
+}
 
 // //
 // $( document ).ready(function() {
