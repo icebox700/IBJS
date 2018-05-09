f42fe576d8e808535bc51afea7abe6f45c8ef2be
Brandon Simms <brandons@iceboxmail.com>
2018-05-09 13:34:54 -0400

    CountFix

--- ts/IBJS.Essent.UI.ts
+++ ts/IBJS.Essent.UI.ts
@@ -37,7 +37,7 @@ $( document ).ready(function() {
 
     // Mini Cart ----------------------------
     $('.cartItemGroup .ItemQty span').each(function() {
-        itemCount = itemCount + Number(this.textContent);
+        itemCount = itemCount + Number($(this)[0].textContent);
     });
     itemCount ? $('#minicart .cart-empty').replaceWith($('.CartInformation')) : $('.CartInformation').css('display', 'none');
     $('.quantity').html(itemCount.toString());
